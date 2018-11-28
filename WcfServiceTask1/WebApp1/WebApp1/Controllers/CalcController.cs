5901398c3a199d5a94a3bdd44f8ac0c9f96b81af
krishna <suvikrish21@gmail.com>
2018-11-28 06:37:31 +0530

    merge mvc and wcf project update

---
+++ WcfServiceTask1/WebApp1/WebApp1/Controllers/CalcController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+       
+
+        public ActionResult Index(Calculations calc)
+        {
+
+            if (!string.IsNullOrEmpty(calc.Operation))
+            {
+                CalcService.CalculatorServiceClient svc = new CalcService.CalculatorServiceClient();
+
+                double result = 0;
+
+                switch(calc.Operation)
+                {
+                    case "+":
+                        result = svc.Add(calc.Number1, calc.Number2);
+                        break;
+
+                    case "-":
+                        result = svc.Subtract(calc.Number1, calc.Number2);
+                        break;
+
+                    case "/":
+                        result = svc.Divide(calc.Number1, calc.Number2);
+                        break;
+
+
+                    case "*":
+                        result = svc.Multiply(calc.Number1, calc.Number2);
+                        break;
+
+
+                }
+                calc.Result = result;
+            }
+
+            return View(calc);
+        }
+    }
+}

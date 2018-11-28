d5c3d8fde9c38e4cf59f596265a6302f9060fb43
krishna <suvikrish21@gmail.com>
2018-11-28 06:29:22 +0530

    mvc app update

---
+++ WebApp1/WebApp1/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Task
+        public string Hello()
+        {
+            TaskService1.Service1Client svc = new TaskService1.Service1Client("BasicHttpBinding_IService1");
+
+
+            return (svc.SayHello(string.Empty));
+        }
+
+        public string Program()
+        {
+
+            TaskService2.Service1Client svc = new TaskService2.Service1Client("NetTcpBinding_IService11");
+            return (svc.TodayProgram(string.Empty));
+        }
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
